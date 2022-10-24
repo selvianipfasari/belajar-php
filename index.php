d9553309926c2d9e0956bae69c1b4a304ba6a974
Selvia Nipfasari <selvianipfasari1901@gmail.com>
2022-10-24 10:56:43 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hello World! <br>";
+
+$nama = "Selvia Nipfasari";
+$umur = 21;
+
+echo "Nama Saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "via";
+$umurAdik = 15;
+
+
+$selisihUmur = $umur - $umurAdik;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+
+
+?>
